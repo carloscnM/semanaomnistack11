aca46f20134a2a6e419d9985949e720806fdd1b8
Carlos Mario Oliveira <oliveiracarlosmario1@gmail.com>
2020-03-27 09:34:51 -0300

    Estrutura projeto mobile

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents';
+import Datail from './pages/Detail';
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Datail" component={Datail} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
